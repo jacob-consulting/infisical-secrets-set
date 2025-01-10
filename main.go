fa648d970a915a67c53ee964a9bcb5cf2e5f77f1
Felix Hummel <felix@felixhummel.de>
2025-01-10 16:07:21 +0100

    --force flag

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -14,6 +13,7 @@ import (
 
 func main() {
 	var env, path string
+	var force bool
 
 	rootCmd := &cobra.Command{
 		Use:   "infisical-secrets-set [OPTIONS] SECRET_NAME",
@@ -43,23 +43,50 @@ func main() {
 
 			secretValue = strings.TrimSpace(secretValue)
 
-			_, err = client.Secrets().Create(infisical.CreateSecretOptions{
-				ProjectID:   os.Getenv("INFISICAL_PROJECT_ID"),
-				Environment: env,
-				SecretKey:   secretName,
-				SecretValue: secretValue,
-				SecretPath:  path,
-			})
+			createSecret := func() error {
+				_, err := client.Secrets().Create(infisical.CreateSecretOptions{
+					ProjectID:   os.Getenv("INFISICAL_PROJECT_ID"),
+					Environment: env,
+					SecretKey:   secretName,
+					SecretValue: secretValue,
+					SecretPath:  path,
+				})
+				return err
+			}
 
+			err = createSecret()
 			if err != nil {
-				fmt.Printf("Failed to set secret: %v\n", err)
-				os.Exit(1)
+				if strings.Contains(err.Error(), "Secret already exist") {
+					if force {
+						fmt.Println("Secret already exists, overwriting due to --force flag.")
+						_, err = client.Secrets().Delete(infisical.DeleteSecretOptions{
+							ProjectID:   os.Getenv("INFISICAL_PROJECT_ID"),
+							Environment: env,
+							SecretKey:   secretName,
+							SecretPath:  path,
+						})
+						if err != nil {
+							fmt.Printf("Failed to delete existing secret: %v\n", err)
+							os.Exit(1)
+						}
+						err = createSecret()
+					} else {
+						fmt.Println("Secret exists. Use --force to overwrite.")
+						os.Exit(1)
+					}
+				}
+
+				if err != nil {
+					fmt.Printf("Failed to set secret: %v\n", err)
+					os.Exit(1)
+				}
 			}
 
 			fmt.Println("Secret set successfully.")
 		},
 	}
 
+	rootCmd.Flags().BoolVar(&force, "force", false, "Overwrite secret if exists")
 	rootCmd.Flags().StringVar(&env, "env", "prod", "Environment to use (dev|stage|prod). Default: prod")
 	rootCmd.Flags().StringVar(&path, "path", "/", "Path to use. Default: /")
 
